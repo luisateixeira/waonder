9f229e71f8cabce2b1b948a7c3f22261c393d7f7
Luisa <luisagpteixeira@gmail.com>
2017-03-29 17:07:40 +0100

    Add mobile wrapper to prevent users to access the site for now

--- src/containers/App.js
+++ src/containers/App.js
@@ -11,9 +11,17 @@ class App extends React.Component {
     const {resetDestinaton, destination} = this.props;
     return (
       <div className="container">
-        <Main />
-        {this.props.children}
-        <Header {...{resetDestinaton, destination}}/>
+        <div className="content">
+          <Main />
+          {this.props.children}
+          <Header {...{resetDestinaton, destination}}/>
+        </div>
+        <div className="mobile">
+          <div className="mobile--wrapper">
+            <div className="mobile--wrapper--logo">wa<del>o</del>nder</div>
+            <div className="mobile--wrapper--warning">Sorry :( <br></br>This app is not yet available for this device.<br></br>Please open it on your desktop.</div>
+          </div>
+        </div>
       </div>
     );
   }
